c3958a349d38618858428068121fb6e8e0b8bbdf
Caetano Sauer <csauer@cs.uni-kl.de>
2018-01-26 17:43:03 +0100

    Archive and SPR fixes

--- src/sm/restart.cpp
+++ src/sm/restart.cpp
@@ -14,35 +14,22 @@ SprIterator::~SprIterator()
 void SprIterator::open(PageID pid)
 {
     archive_scan.open(pid, pid+1);
-    replayed_count = 0;
-}
-
-bool SprIterator::next(logrec_t*& lr)
-{
-    if (archive_scan.next(lr)) {
-        replayed_count++;
-        return true;
-    }
-    return false;
 }
 
 void SprIterator::apply(fixable_page_h &p)
 {
-    lsn_t prev_lsn = lsn_t::null;
     PageID pid = p.pid();
     logrec_t* lr;
 
-    while (next(lr)) {
+    while (archive_scan.next(lr)) {
         w_assert1(lr->valid_header());
+        w_assert1(lr->is_redo());
         w_assert1(lr->page_version() > 0);
+        w_assert1(pid == lr->pid());
+        w_assert1(lr->has_page_img() || p.version() > 0);
+        w_assert1(p.version() < lr->page_version()); // Always true in FineLine
 
-        if (lr->is_redo() && p.version() < lr->page_version()) {
-            DBGOUT1(<< "SPR page(" << p.pid()
-                    << ") version=" << p.version() << ", log=" << *lr);
-
-            w_assert1(pid == lr->pid());
-            lr->redo(&p);
-        }
+        lr->redo(&p);
     }
 }
 

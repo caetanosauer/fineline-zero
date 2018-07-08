d7c67017320dcaa8b4c67991008a148938389f1d
Caetano Sauer <csauer@cs.uni-kl.de>
2018-07-08 11:56:28 +0200

    Increased redo buffer size for tpcc load transactions

--- src/sm/smthread.h
+++ src/sm/smthread.h
@@ -203,8 +203,8 @@ class smthread_t {
         logrec_t _logbuf;
         logrec_t _logbuf2; // for "piggy-backed" SSX
         UndoBuffer _undo_buf;
-        static constexpr size_t BufferSize = 1024 * 1024;
-        RedoBuffer _redo_buf{BufferSize};
+        static constexpr size_t RedoBufferSize = 10 * 1024 * 1024;
+        RedoBuffer _redo_buf{RedoBufferSize};
 
         xct_t*   xct;
         int      pin_count;      // number of rsrc_m pins
